4ef98b29b0cdb8e42b90d13279b7cb2b5a000b02
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2021-11-19 11:30:57 +0530

    Create stroll_park.c

---
+++ stroll_park.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define p while(j<k)
+#define pp while(t--)
+int main()
+{ int t;
+scanf("%d",&t);
+pp{
+    int a,b,c,k,x=0;
+    scanf("%d%d%d%d",&a,&b,&c,&k);
+    int j=0;
+    p{
+    j=a*x*x + b*x +c;
+    x++;
+   // if(j>=k){
+//break;
+  //  }
+}
+
+    printf("%d\n",x-1);
+}
+	return 0;
+}
