0a965e8e4bfe3b350c1ed06e41717c2796f471f4
VISHAL DUBEY <77582656+vishaldubey40@users.noreply.github.com>
2022-01-15 13:48:19 +0530

    Create pre_order_transversal_binary_tree.c

---
+++ pre_order_transversal_binary_tree.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node* left;
+    struct node* right;
+};
+struct node* createnode(int a){
+    struct node* n;
+    n=(struct node*)malloc(sizeof(struct node));
+    n->data=a;
+    n->left=NULL;
+    n->right=NULL;
+    return n;
+}
+void preorder(struct node* root){
+    if(root!=NULL){
+        printf("%d",root->data);
+    
+    preorder(root->left);
+    preorder(root->right);
+}
+}
+int main() {
+    struct node* p=createnode(4);
+    struct node *p1=createnode(3);
+  struct node *p2=createnode(9);
+  struct node *p3=createnode(1);
+  struct node *p4=createnode(5);
+  struct node *p5=createnode(7);
+    p->left=p1;                           //            4
+    p->right=p2;                          //         3      9
+    p1->left=p3;                           //      1   5   7
+    p1->right=p4;
+    p2->right=p5;
+    preorder(p);
+}
